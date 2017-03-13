044288a640ce36bfb5f3ba33a71f2fe077c2d591
Henry Pan <henry-pan@users.noreply.github.com>
2017-03-12 18:52:44 -0700

    85% win, avg score 4.67

--- myTeam.py
+++ myTeam.py
@@ -85,9 +85,9 @@ class ReflexCaptureAgent(CaptureAgent):
 
 class AggressiveAgent(ReflexCaptureAgent):
   """
-  A reflex agent that seeks food. This is an agent
-  we give you to get an idea of what an offensive agent might look like,
-  but it is by no means the best or only way to build an offensive agent.
+  A reflex agent that seeks food. The agents will split up and target
+  different halves of the map. While in ghost form, they will make
+  an attempt at defense but will resume aggression as soon as possible.
   """
   def getFeatures(self, gameState, action):
     features = util.Counter()
@@ -141,51 +141,18 @@ class AggressiveAgent(ReflexCaptureAgent):
 
     return features
 
+
   def getWeights(self, gameState, action):
-    return {'successorScore': 100, 'distanceToFood': -1, 'invaderDistance': -5, 'numInvaders': -1000, 'ghostDistance': 1, 'scaredGhost': -1.2}
+    return {'successorScore': 100, 'distanceToFood': -1, 'invaderDistance': -5, 'numInvaders': -1000, 'ghostDistance': 0.9, 'scaredGhost': -1.2}
+
+
 
 class DummyAgent(CaptureAgent):
-  """
-  A Dummy agent to serve as an example of the necessary agent structure.
-  You should look at baselineTeam.py for more details about how to
-  create an agent as this is the bare minimum.
-  """
 
   def registerInitialState(self, gameState):
-    """
-    This method handles the initial setup of the
-    agent to populate useful fields (such as what team
-    we're on). 
-    
-    A distanceCalculator instance caches the maze distances
-    between each pair of positions, so your agents can use:
-    self.distancer.getDistance(p1, p2)
-
-    IMPORTANT: This method may run for at most 15 seconds.
-    """
-
-    ''' 
-    Make sure you do not delete the following line. If you would like to
-    use Manhattan distances instead of maze distances in order to save
-    on initialization time, please take a look at
-    CaptureAgent.registerInitialState in captureAgents.py. 
-    '''
     CaptureAgent.registerInitialState(self, gameState)
 
-    ''' 
-    Your initialization code goes here, if you need any.
-    '''
-
-
   def chooseAction(self, gameState):
-    """
-    Picks among actions randomly.
-    """
     actions = gameState.getLegalActions(self.index)
-
-    ''' 
-    You should change this in your own agent.
-    '''
-
     return random.choice(actions)
 
